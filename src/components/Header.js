3b49c50cb64d347766674130829934ce6d73e412
Jonathan Cheng <jcheng461@gmail.com>
2023-12-10 22:59:40 -0800

    WIP: add user log in status to show persistently; add a user profile display

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header>
-      <h1>Inventory Tracker</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/signin">Sign In</Link>
-        </li>
-      </ul>
+      <div>
+        <h1>Inventory Tracker</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signin">Sign In</Link>
+          </li>
+        </ul>
+      </div>
       <hr />
     </header>
   );
---
+++ src/components/UserCheckedOutItems.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+// Show a list of what the user has in possession
